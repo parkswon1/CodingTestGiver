e32b25c15eee6bf3b21c079e2ead1f43729711b6
parkseokwon <parkswon1@naver.com>
2024-05-30 23:42:07 +0900

    Count(u)를 사용해서 (username은 백준에서 아예 중복을 막았기 떄문에 중복이 무조건 없다) 있다면 1 없다면 0을 return하는 findByName메소드 생성

--- AlgoShuffer/src/main/java/com/example/algoshuffer/repository/UserRepository.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.example.algoshuffer.repository;
 
 import com.example.algoshuffer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT Count(u) FROM User u WHERE u.username = :username")
+    public int findByName(@Param("username") String username);
 }
