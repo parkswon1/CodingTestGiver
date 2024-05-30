43bc8703581e7aa2dbc8387d2ac06bd52e796be2
parkseokwon <parkswon1@naver.com>
2024-05-30 23:43:47 +0900

    Getter NoArgsConstructor 어노테이션 생성

--- AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Problem.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Problem.java
@@ -1,14 +1,17 @@
 package com.example.algoshuffer.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Problem {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
