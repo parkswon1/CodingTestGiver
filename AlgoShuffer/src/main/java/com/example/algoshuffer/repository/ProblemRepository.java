ee6a9c494359fed3dd7fc5448a3fc3a01e02d686
parkseokwon <parkswon1@naver.com>
2024-05-30 23:44:19 +0900

    리파지토리 어노테이션 까먹어서 추가

--- AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
@@ -2,6 +2,8 @@ package com.example.algoshuffer.repository;
 
 import com.example.algoshuffer.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
+@Repository
+public interface ProblemRepository extends JpaRepository<Problem, Long>{
 }
