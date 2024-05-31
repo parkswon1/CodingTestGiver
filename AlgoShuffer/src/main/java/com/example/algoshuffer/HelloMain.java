8189d18cf14b7bc688b8474d172041da52aa0afd
parkseokwon <parkswon1@naver.com>
2024-05-31 23:30:33 +0900

    solved.ac API 메소드 3개 작성 사용자 이름으로 사용자 정보 가져오기 사용자 이름으로 사용자가 푼 문제 가져오기 백준 문제 숫자열로 가져오기

--- AlgoShuffer/src/main/java/com/example/algoshuffer/HelloMain.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/HelloMain.java
@@ -1,20 +1,33 @@
 package com.example.algoshuffer;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class HelloMain {
     public static void main(String[] args) throws Exception {
-        String username = "parkswon1";
+        //String uri = getProblemByArray(List.of(1000));
+        //String uri = getUserSolvedProblemByName("parkswon1");
+        String uri = getUserByName("parkswon1");
 
+        // HTTP 요청 생성
+        solvedacAPIRequest(uri);
+    }
+
+    //uri요청이 있을시 요청을 받아주고 데이터 받아옴
+    private static void solvedacAPIRequest(String uri) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://solved.ac/api/v3/search/problem?query=s%40parkswon1"))
+                .uri(URI.create(uri))
+                .header("x-solvedac-language", "ko")
                 .header("Accept", "application/json")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .GET()
                 .build();
 
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
@@ -26,5 +39,29 @@ public class HelloMain {
         // 객체를 다시 JSON 문자열로 변환하여 출력
         String prettyJson = gson.toJson(jsonObject);
         System.out.println(prettyJson);
+    }
+
+    //사용자 이름으로 사용자 정보 가져오기
+    private static String getUserByName(String User) {
+        String uri = "https://solved.ac/api/v3/search/user?query=" + User;
+        return uri;
+    }
+
+
+    //사용자 이름으로 사용자가 푼 문제 가져오기
+    private static String getUserSolvedProblemByName(String User) {
+        String uri = "https://solved.ac/api/v3/search/problem?query=s%40" + User;
+        return uri;
+    }
+
+
+    //백준 문제 숫자열로 가져오기
+    private static String getProblemByArray(List<Integer> problemIds) {
+        String problemIdsParam = problemIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        String uri = "https://solved.ac/api/v3/problem/lookup?problemIds=" + problemIdsParam;
+        return uri;
     }
 }
