51a8ddee9d539458631c0745c427877b01e9a923
parkseokwon <parkswon1@naver.com>
2024-06-03 21:34:16 +0900

    column 매칭

--- AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Problem.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Problem.java
@@ -3,23 +3,29 @@ package com.example.algoshuffer.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Problem {
     @Id
+    @Column(name = "problem_id")
     private Long problemid;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "level")
     private int level;
 
-    @ElementCollection
-    private Set<String> tags = new HashSet<>();
+    @ManyToMany
+    @Column(name = "tags")
+    private Set<ProblemTag> bojTag = new HashSet<>();
 
     @ManyToMany
     private Set<User> solvedByUsers = new HashSet<>();
