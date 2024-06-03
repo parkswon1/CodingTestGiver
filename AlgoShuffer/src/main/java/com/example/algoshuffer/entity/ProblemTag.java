b258b92ac294c57d87017ea6b80511df0a5c7242
parkseokwon <parkswon1@naver.com>
2024-06-03 22:48:09 +0900

    메핑 테이블 아직 생성안했기 때문에 ManyToMany잠시뺐음

--- AlgoShuffer/src/main/java/com/example/algoshuffer/entity/ProblemTag.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/entity/ProblemTag.java
@@ -1,18 +1,15 @@
 package com.example.algoshuffer.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.HashSet;
-import java.util.Set;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "problem_tag")
+@ToString
 public class ProblemTag {
     @Id
     @Column(name = "boj_tag_id")
@@ -21,11 +18,9 @@ public class ProblemTag {
     @Column(name = "display_name")
     private String displayName;
 
-    @Column(name = "display_name_short")
+    @Column(name = "display_name_sort")
     private String displayNameShort;
 
+    @Column(name = "key")
     private String key;
-
-    @ManyToMany
-    private Set<Problem> problems = new HashSet<>();
 }
