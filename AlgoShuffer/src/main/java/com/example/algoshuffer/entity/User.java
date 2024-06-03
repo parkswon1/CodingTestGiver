e28af65be47273619a962585a3b7d757ec789b1a
parkseokwon <parkswon1@naver.com>
2024-06-03 21:35:10 +0900

    column 매칭

--- AlgoShuffer/src/main/java/com/example/algoshuffer/entity/User.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/entity/User.java
@@ -3,6 +3,7 @@ package com.example.algoshuffer.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,11 +11,14 @@ import java.util.Set;
 @Entity
 @Getter
 @NoArgsConstructor
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "username")
     private String username;
 
     @ManyToMany
