94c7de1c051a5c85615987591336dc685ccc5c95
parkseokwon <parkswon1@naver.com>
2024-06-10 22:14:33 +0900

    sovledacAPIReqeuset메소드의 return방식을 JsonObject로 넘겨주던 기존 방식에서 HttpResponse로 넘겨줘서 다른 메소드가 이 메소드를 사용할때 좀더 자유롭게 사용할수 있도록 변환

--- AlgoShuffer/src/main/java/com/example/algoshuffer/SolvedAPI.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/SolvedAPI.java
@@ -13,7 +13,7 @@ import com.google.gson.JsonParser;
 
 public class SolvedAPI {
     //uri요청이 있을시 요청을 받아주고 데이터 받아옴
-    public static JsonObject solvedacAPIRequest(String uri) throws IOException, InterruptedException {
+    public static HttpResponse<String> solvedacAPIRequest(String uri) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("x-solvedac-language", "ko")
@@ -23,10 +23,7 @@ public class SolvedAPI {
 
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
-        // JSON 데이터
-        JsonParser parser = new JsonParser();
-
-        return parser.parse(response.body()).getAsJsonObject();
+        return response;
     }
 
     //사용자 이름으로 사용자 정보 가져오기
