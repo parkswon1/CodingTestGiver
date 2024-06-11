3d050e54c93d7774e26c687b74e6db07e5fa8e8d
parkseokwon <parkswon1@naver.com>
2024-06-11 13:11:51 +0900

    1. tag를 controller에서 받아올때 JsonObejct로 받는게 아니라 HttpResponse로 받기 때문에 따로 수정하는 걸로 변경
    2. 문제를 sovled api에서 받아올때 50개씩 나눠서 1000번부터 31980까지 받아오도록 변경

--- AlgoShuffer/src/main/java/com/example/algoshuffer/controller/SolvedAPIController.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/controller/SolvedAPIController.java
@@ -1,27 +1,56 @@
 package com.example.algoshuffer.controller;
 
 import com.example.algoshuffer.SolvedAPI;
+import com.example.algoshuffer.service.ProblemService;
 import com.example.algoshuffer.service.ProblemTagService;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("solved")
 public class SolvedAPIController {
     private final ProblemTagService problemTagService;
+    private final ProblemService problemService;
+    public static JsonParser parser = new JsonParser();
 
     //24-06-03날자상 206개의 tag존재 5page까지 가져오면 됨
     @GetMapping("/tag")
     public void fetchAndSaveProblemTag() throws IOException, InterruptedException {
         for (int page = 1; page < 6; page++){
-            JsonObject tags = SolvedAPI.solvedacAPIRequest(SolvedAPI.getTag(page));
+            HttpResponse<String> response = SolvedAPI.solvedacAPIRequest(SolvedAPI.getTag(page));
+            JsonObject tags = parser.parse(response.body()).getAsJsonObject();
             problemTagService.saveProblemTags(tags);
+        }
+    }
+
+    @GetMapping("/problem")
+    public void fetchAndSaveProblem() throws IOException, InterruptedException {
+        int start = 1000;
+        int end = 31980;
+        int batchSize = 50;
+
+        for (int i = start; i <= end; i += batchSize) {
+            List<Integer> problems = new ArrayList<>();
+            for (int j = i; j < i + batchSize && j <= end; j++) {
+                problems.add(j);
+            }
+
+            String uri = SolvedAPI.getProblemByArray(problems);
+            HttpResponse<String> response = SolvedAPI.solvedacAPIRequest(uri);
+            JsonArray JsonProblems = parser.parse(response.body()).getAsJsonArray();
+            problemService.saveProblems(JsonProblems);
         }
     }
 }
