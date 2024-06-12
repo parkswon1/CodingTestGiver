531bf6eca3261e8b6ec7b5778bc93115bc283675
parkseokwon <parkswon1@naver.com>
2024-06-13 02:10:53 +0900

    아직 client쪽 로직을 구현을 안해서 username을 일단 내 solved.ac아이디로 넣어두고 만들었다. 문제를 가져올때는 한번에 가져올 수 있었지만 user가 푼문제경우 page를 나눠서 가져와야 한다. 처음에 1페이지를 받아오면 총 문제의 개수를 알려주는데 한번에 50개씩 보내줘서 총 문제의 개수를 50으로 나누고 +1해서 모든 페이지의 문제를 읽어올 수 있도록 변경했다.

--- AlgoShuffer/src/main/java/com/example/algoshuffer/controller/SolvedAPIController.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/controller/SolvedAPIController.java
@@ -1,13 +1,15 @@
 package com.example.algoshuffer.controller;
 
 import com.example.algoshuffer.SolvedAPI;
+import com.example.algoshuffer.entity.User;
+import com.example.algoshuffer.service.Mapping.UserProblemMappingService;
 import com.example.algoshuffer.service.ProblemService;
 import com.example.algoshuffer.service.ProblemTagService;
+import com.example.algoshuffer.service.UserService;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,8 @@ import java.util.List;
 public class SolvedAPIController {
     private final ProblemTagService problemTagService;
     private final ProblemService problemService;
+    private final UserService userService;
+    private final UserProblemMappingService userProblemMappingService;
     public static JsonParser parser = new JsonParser();
 
     //24-06-03날자상 206개의 tag존재 5page까지 가져오면 됨
@@ -35,6 +39,7 @@ public class SolvedAPIController {
         }
     }
 
+    //문제를 1000번문제부터 31980번째 문제까지 50개씩 묶어서 가져옴
     @GetMapping("/problem")
     public void fetchAndSaveProblem() throws IOException, InterruptedException {
         int start = 1000;
@@ -51,6 +56,31 @@ public class SolvedAPIController {
             HttpResponse<String> response = SolvedAPI.solvedacAPIRequest(uri);
             JsonArray JsonProblems = parser.parse(response.body()).getAsJsonArray();
             problemService.saveProblems(JsonProblems);
+        }
+    }
+
+    //user를 입력하면 user이름을 받아와서 user가 푼문제를 저장
+    @GetMapping("/user")
+    public void fetchAndSaveUser() throws IOException, InterruptedException{
+        String username = "parkswon1";
+        int page = 1;
+        User user = userService.findByName(username);
+        if (user == null){
+            userService.saveUserByName(username);
+            user = userService.findByName(username);
+        }
+
+        String uri = SolvedAPI.getUserSolvedProblemByName(username, page);
+        HttpResponse<String> response = SolvedAPI.solvedacAPIRequest(uri);
+        JsonObject JsonUser = parser.parse(response.body()).getAsJsonObject();
+        userProblemMappingService.saveUserProblemMapping(JsonUser, user);
+
+        int endPage = (JsonUser.get("count").getAsInt() / 50) + 1;
+        for (page = 2; page <= endPage; page++){
+            uri = SolvedAPI.getUserSolvedProblemByName(username, page);
+            response = SolvedAPI.solvedacAPIRequest(uri);
+            JsonUser = parser.parse(response.body()).getAsJsonObject();
+            userProblemMappingService.saveUserProblemMapping(JsonUser, user);
         }
     }
 }
