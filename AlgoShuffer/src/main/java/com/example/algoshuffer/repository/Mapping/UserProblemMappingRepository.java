13a5bc65b20d5eb30091c072be71286d152df36d
parkseokwon <parkswon1@naver.com>
2024-06-13 02:18:02 +0900

    user객체를 받아와서 user랑 관련된 문제를 List로 반환 사실 Set<Long>으로 반환시켜서 service에서 형변환을 안하게 시키고 싶었는데 JPA에서 set형 반환을 하지 않는다고하다.

--- AlgoShuffer/src/main/java/com/example/algoshuffer/repository/Mapping/UserProblemMappingRepository.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/repository/Mapping/UserProblemMappingRepository.java
@@ -4,12 +4,9 @@ import com.example.algoshuffer.entity.Mapping.UserProblemMapping;
 
 import com.example.algoshuffer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface UserProblemMappingRepository extends JpaRepository<UserProblemMapping, Long> {
