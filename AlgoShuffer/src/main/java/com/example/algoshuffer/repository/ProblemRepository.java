520d7155e8294a6a204bdce11335c34d2284d328
parkseokwon <parkswon1@naver.com>
2024-06-24 15:59:03 +0900

    tag를 사용하기위한 수정
    1. 태그와 사용자의 id를 받아서 사용자가 푼 문제들을 찾아 반환하는 메서드
    
    2. 사용자가 해결한 문제를 제외하고 특정 태그가 있는 문제를 찾는 메세드 평균 tier에 가까운 순서대로 정렬되고, 평균 tier에서 같은 거리에 있는 경우, 수락된 사용자 수가 많은 순서대로 정렬

--- AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
@@ -1,10 +1,28 @@
 package com.example.algoshuffer.repository;
 
 import com.example.algoshuffer.entity.Problem;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long>{
     boolean existsByProblemId(Long problemId);
+
+    // 태그와 사용자의 id를 받아서 사용자가 푼 문제들을 찾아 반환하는 메서드
+    @Query("SELECT p FROM Problem p JOIN p.userProblemMappings upm JOIN p.problemTagMappings ptm " +
+            "WHERE upm.user.id = :userId AND ptm.problemTag.bojTagId = :tagId")
+    List<Problem> findSolvedProblemsByUserAndTag(@Param("userId") Long userId, @Param("tagId") Long tagId);
+
+    // 사용자가 해결한 문제를 제외하고 특정 태그가 있는 문제를 찾는 메세드
+    // 평균 tier에 가까운 순서대로 정렬되고, 평균 tier에서 같은 거리에 있는 경우, 수락된 사용자 수가 많은 순서대로 정렬
+    @Query("SELECT p FROM Problem p JOIN p.problemTagMappings ptm " +
+            "WHERE ptm.problemTag.bojTagId = :tagId AND p.problemId NOT IN " +
+            "(SELECT upm.problem.problemId FROM UserProblemMapping upm WHERE upm.user.id = :userId) " +
+            "ORDER BY ABS(p.level - :averageTier) ASC, p.acceptedUserCount DESC")
+    List<Problem> findProblemsByTagExcludingUserSolved(@Param("userId") Long userId, @Param("tagId") Long tagId, @Param("averageTier") int averageTier, Pageable pageable);
 }
