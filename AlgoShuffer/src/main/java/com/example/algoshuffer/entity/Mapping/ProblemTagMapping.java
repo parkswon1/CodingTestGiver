09a6a15cb925d8210a9a8264d565f4df979186c9
parkseokwon <parkswon1@naver.com>
2024-06-24 13:38:53 +0900

    @JsonIgnore 추가

--- AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Mapping/ProblemTagMapping.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/entity/Mapping/ProblemTagMapping.java
@@ -2,6 +2,7 @@ package com.example.algoshuffer.entity.Mapping;
 
 import com.example.algoshuffer.entity.Problem;
 import com.example.algoshuffer.entity.ProblemTag;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,9 +22,11 @@ public class ProblemTagMapping implements Serializable {
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "problem_id", nullable = false)
+    @JsonIgnore
     private Problem problem;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "boj_tag_id", nullable = false)
+    @JsonIgnore
     private ProblemTag problemTag;
 }
