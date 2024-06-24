bbfb9ea229eb7297c22a98656fe26f5b67042669
parkseokwon <parkswon1@naver.com>
2024-06-24 20:43:24 +0900

    사용자의 이름을 받아서 추천문제 10개를 반환하는 메소드

--- AlgoShuffer/src/main/java/com/example/algoshuffer/restController/RecommendController.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/restController/RecommendController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//문제를 추천해주는 컨트롤러들을 모은 컨트롤러
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recommend")
@@ -16,10 +17,17 @@ public class RecommendController {
     private final UserService userService;
     private final ProblemService problemService;
 
+    //user의 이름과 tagid를 입력하면 문제를 5개 반환
     @GetMapping("/{username}/tag/{tagId}")
     public List<Problem> getProblemByUsernameAndTag(@PathVariable String username,@PathVariable Long tagId){
         User user = userService.findByName(username);
-        List<Problem> tagProblem = problemService.findByUserAndTag(user, tagId);
-        return tagProblem;
+        return problemService.findProblemByUserAndTag(user, tagId);
+    }
+
+    //user의 이름을 입력하면 문제를 10 개 반환
+    @GetMapping("/{username}")
+    public List<Problem> getProblemByUesrname(@PathVariable String username){
+        User user = userService.findByName(username);
+        return problemService.findProblemByUser(user);
     }
 }
