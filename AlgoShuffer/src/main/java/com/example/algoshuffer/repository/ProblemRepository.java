8111611c8261d02b37b7cabde2e44fcf92ca9b57
parkseokwon <parkswon1@naver.com>
2024-06-24 20:33:50 +0900

    사용자가 해결한 문제랑 비슷한 문제를 찾는 메서드 사용자 티어에 2정도 차이나는 곳으로 문제를 묶고 문제를 맞은 사용자 수가 많은 순서대로 정렬

--- AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
+++ AlgoShuffer/src/main/java/com/example/algoshuffer/repository/ProblemRepository.java
@@ -19,10 +19,17 @@ public interface ProblemRepository extends JpaRepository<Problem, Long>{
     List<Problem> findSolvedProblemsByUserAndTag(@Param("userId") Long userId, @Param("tagId") Long tagId);
 
     // 사용자가 해결한 문제를 제외하고 특정 태그가 있는 문제를 찾는 메세드
-    // 평균 tier에 가까운 순서대로 정렬되고, 평균 tier에서 같은 거리에 있는 경우, 수락된 사용자 수가 많은 순서대로 정렬
+    // 평균 tier에 가까운 순서대로 정렬되고, 평균 tier에서 같은 거리에 있는 경우, 문제를 맞은 사용자 수가 많은 순서대로 정렬
     @Query("SELECT p FROM Problem p JOIN p.problemTagMappings ptm " +
             "WHERE ptm.problemTag.bojTagId = :tagId AND p.problemId NOT IN " +
             "(SELECT upm.problem.problemId FROM UserProblemMapping upm WHERE upm.user.id = :userId) " +
             "ORDER BY ABS(p.level - :averageTier) ASC, p.acceptedUserCount DESC")
     List<Problem> findProblemsByTagExcludingUserSolved(@Param("userId") Long userId, @Param("tagId") Long tagId, @Param("averageTier") int averageTier, Pageable pageable);
+
+    // 사용자가 해결한 문제를 제외하고 사용자의 tier랑 비슷한 문제를 찾는 메서드
+    // 사용자 티어에 2정도 차이나는 곳으로 문제를 묶고 문제를 맞은 사용자 수가 많은 순서대로 정렬
+    @Query("SELECT p FROM Problem p WHERE p.level BETWEEN :tier - 2 AND :tier + 2 " +
+            "AND p.problemId NOT IN (SELECT upm.problem.problemId FROM UserProblemMapping upm WHERE upm.user.id = :userId) " +
+            "ORDER BY p.acceptedUserCount DESC")
+    List<Problem> findProblemByUser(@Param("userId") Long userId, @Param("tier") int tier, Pageable pageable);
 }
